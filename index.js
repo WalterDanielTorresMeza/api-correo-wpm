03bdfef6053651d2080ab0c334439d2ed9033ea3
Walter Daniel Torres Meza <walterdanieltorresmeza@gmail.com>
2025-06-24 16:05:48 -0600

    Primer commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.post('/enviar', async (req, res) => {
+  const { to, subject, html } = req.body;
+
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'walterdanieltorresmeza@gmail.com',
+        pass: 'eczp yuex ojcl saod',
+      },
+    });
+
+    const info = await transporter.sendMail({
+      from: '"Notificador WPM" <walter.torres@outliersconsulting.com>',
+      to,
+      subject,
+      html,
+    });
+
+    res.json({ success: true, messageId: info.messageId });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('🚀 API corriendo en http://localhost:3000');
+});
